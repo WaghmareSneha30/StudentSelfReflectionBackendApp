27c4c5ae495fae64e4f40ae0a5c8764d830eef8d
HP <snehawaghmare3009@gmail.com>
2025-04-17 16:40:50 +0530

    Getting Started With StudentSelfReflection Backend Project

---
+++ src/main/java/com/sneha/StudentSelfReflection/StudentSelfReflectionApplication.java
@@ -0,0 +1,13 @@
+package com.sneha.StudentSelfReflection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSelfReflectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSelfReflectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneha/StudentSelfReflection/StudentSelfReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.StudentSelfReflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSelfReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
